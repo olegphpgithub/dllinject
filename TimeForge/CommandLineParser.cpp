b836c66c6dc1bfc0cfa639ba2b1021a2becc1e0d
OlegKorzun <OKorzun@ru.netnucleus.com>
2025-09-08 14:51:24 +0300

    Change command line;

--- TimeForge/CommandLineParser.cpp
+++ TimeForge/CommandLineParser.cpp
@@ -16,101 +16,39 @@
 
 bool CommandLineParser::Parse(int argc, char* argv[])
 {
-    CLI::App app{"Example with CLI11"};
+    CLI::App app{"CLI11"};
     app.allow_extras();
 
     app.add_flag("--datetime", target_datetime, "Datetime");
-    app.add_flag("--application", target_application, "Application");
-    bool parameters_mode = false;
-    app.add_flag("--parameters", parameters_mode, "Parameters");
 
     CLI11_PARSE(app, argc, argv);
 
-    std::vector<std::string> names;
-    if (parameters_mode) {
-        for (auto &arg : app.remaining()) {
-            names.push_back(arg);
+    std::vector<std::string> parameters;
+
+    for (auto &arg : app.remaining()) {
+        if (target_application.empty()) {
+            target_application = std::string(arg);
+        }
+        else {
+            parameters.push_back(arg);
         }
     }
-
-    std::cout << "App1908:" << target_application << "\n";
 
     std::stringstream command_line_stream;
     std::string delimiter("");
     std::string border("");
-    for (const std::string &n : names) {
+    for (const std::string &p : parameters) {
         delimiter = command_line_stream.str().empty() ? "" : " ";
-        border = n.find(' ') != std::string::npos ? "\"" : "";
-        command_line_stream << delimiter << border << n << border;
+        border = p.find(' ') != std::string::npos ? "\"" : "";
+        command_line_stream << delimiter << border << p << border;
     }
     command_line = command_line_stream.str();
-
-    std::cout << "cl:" << command_line << "\n";
-
-    /*
-    const char* const short_opts = "a:p:d:";
-    const option long_opts[] = {
-            {"application", required_argument, nullptr, 'a'},
-            {"parameter", required_argument, nullptr, 'p'},
-            {"datetime", required_argument, nullptr, 'd'},
-            {"help", no_argument, nullptr, 'h'},
-            {nullptr, no_argument, nullptr, 0}
-    };
-
-    std::ostringstream command_line_stream;
-
-    while (true)
-    {
-        const auto opt = getopt_long(argc, argv, short_opts, long_opts, nullptr);
-
-        if (-1 == opt) {
-            break;
-        }
-
-        std::string border("");
-
-        switch (opt)
-        {
-        case 'a':
-            target_application = std::string(optarg);
-            break;
-
-        case 'p':
-            if (!command_line_stream.str().empty()) {
-                command_line_stream << " ";
-            }
-
-            if (strchr(optarg, ' ')) {
-                border = "\"";
-            }
-            else {
-                border = "";
-            }
-
-            command_line_stream << border << std::string(optarg) << border;
-            break;
-
-        case 'd':
-            target_datetime = std::string(optarg);
-            break;
-
-        case 'h':
-        case '?':
-        default:
-            PrintHelp();
-            return false;
-        }
-    }
-    command_line = command_line_stream.str();
-    */
 
     return ValidateArguments();
 }
 
 bool CommandLineParser::ValidateArguments()
 {
-    std::cout << "command_line:" << command_line.c_str() << std::endl;
-
     if (target_application.empty() || target_datetime.empty()) {
         PrintHelp();
         return false;
@@ -176,8 +114,5 @@ WORD CommandLineParser::StringToWord(const char* str) {
 
 void CommandLineParser::PrintHelp()
 {
-    std::cout <<
-        "--application <path>\n"
-        "--datetime <0000-00-00T00:00:00>\n"
-        "--help\n";
+    std::cout << "--datetime=<0000-00-00T00:00:00> <path to the application> <parameters>" << std::endl;
 }
